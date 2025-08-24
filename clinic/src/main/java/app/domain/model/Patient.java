08340493f67ad33f5c8e0735d62051838413f5f6
fttmatteo <valenciaardila988@icloud.com>
2025-08-23 21:21:16 -0500

    add enums and entities

---
+++ clinic/src/main/java/app/domain/model/Patient.java
@@ -0,0 +1,169 @@
+package app.domain.model;
+
+import app.domain.model.emuns.Role;
+import java.sql.Date;
+
+public class Patient{
+    private long id;
+    private String fullName;
+    private long document;
+    private int age;
+    private Date birth;
+    private String gender;
+    private String address;
+    private int telephone;
+    private String email;
+    private String emergencyFirstName;
+    private String emergencyLastName;
+    private String relationShip;
+    private int emergencyContact;
+    private String companyName;
+    private long companyNumber;
+    private boolean status;
+    private Date validity;
+    private Role role;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public long getDocument() {
+        return document;
+    }
+
+    public void setDocument(long document) {
+        this.document = document;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmergencyFirstName() {
+        return emergencyFirstName;
+    }
+
+    public void setEmergencyFirstName(String emergencyFirstName) {
+        this.emergencyFirstName = emergencyFirstName;
+    }
+
+    public String getEmergencyLastName() {
+        return emergencyLastName;
+    }
+
+    public void setEmergencyLastName(String emergencyLastName) {
+        this.emergencyLastName = emergencyLastName;
+    }
+
+    public String getRelationShip() {
+        return relationShip;
+    }
+
+    public void setRelationShip(String relationShip) {
+        this.relationShip = relationShip;
+    }
+
+    public int getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(int emergencyContact) {
+        this.emergencyContact = emergencyContact;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public long getCompanyNumber() {
+        return companyNumber;
+    }
+
+    public void setCompanyNumber(long companyNumber) {
+        this.companyNumber = companyNumber;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getValidity() {
+        return validity;
+    }
+
+    public void setValidity(Date validity) {
+        this.validity = validity;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
---
+++ clinic/src/main/java/app/domain/model/emuns/Role.java
@@ -0,0 +1,8 @@
+package app.domain.model.emuns;
+
+public enum Role {
+    MEDIC,
+    NURSE,
+    ADMIN,
+    HUMAN_RESOURCES
+}
