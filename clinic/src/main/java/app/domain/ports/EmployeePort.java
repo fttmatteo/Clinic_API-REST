97f9388b5ee0457c8593addb4480f0e7146f8af5
fttmatteo <valenciaardila988@icloud.com>
2025-09-21 12:28:57 -0500

    add delete employee

--- clinic/src/main/java/app/domain/ports/EmployeePort.java
+++ clinic/src/main/java/app/domain/ports/EmployeePort.java
@@ -7,4 +7,5 @@ public interface EmployeePort {
     public Employee findByDocument(int document) throws Exception;
     public Employee findByUsername(String username) throws Exception;
     public Employee save(Employee employee) throws Exception;
+    public void deleteByDocument(int document) throws Exception;
 }
