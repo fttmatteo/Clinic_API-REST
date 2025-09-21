7e772cabbc8603c32d6e8698bb703174829b69af
fttmatteo <valenciaardila988@icloud.com>
2025-09-20 22:51:23 -0500

    add file procedure and delete files createProcedureMedication and procedureMedicationPort

--- clinic/src/main/java/app/domain/ports/ProcedureMedicationPort.java
+++
@@ -1,8 +0,0 @@
-package app.domain.ports;
-
-import app.domain.model.ProcedureMedication;
-
-public interface ProcedureMedicationPort {
-    ProcedureMedication findById(ProcedureMedication procedureMedication) throws Exception;
-    void save(ProcedureMedication procedureMedication) throws Exception;
-}
--- clinic/src/main/java/app/domain/services/CreateClinicalOrder.java
+++ clinic/src/main/java/app/domain/services/CreateClinicalOrder.java
@@ -17,23 +17,5 @@ public class CreateClinicalOrder {
     }
 
     public void create(ClinicalOrder clinicalOrder) throws Exception {
-        if (clinicalOrder.getMedic() == null || clinicalOrder.getMedic().getDocument() == 0) {
-            throw new Exception("Debe indicar el documento del médico");
-        }
-        if (clinicalOrder.getPatient() == null || clinicalOrder.getPatient().getDocument() == 0) {
-            throw new Exception("Debe indicar el documento del paciente");
-        }
-        Patient patientQuery = new Patient();
-        patientQuery.setDocument(clinicalOrder.getPatient().getDocument());
-        Patient patient = patientPort.findByDocument(patientQuery);
-        if (patient == null) {
-            throw new Exception("La orden debe asociarse a un paciente registrado");
-        }
-
-        if (clinicalOrder.getDate() == null) {
-            clinicalOrder.setDate(new Date(System.currentTimeMillis()));
-        }
-
-        clinicalOrderPort.save(clinicalOrder);
-    }
+    }       
 }
