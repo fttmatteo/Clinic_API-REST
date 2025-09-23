fe4871d300b17c8ddde29f909ba0f8bbbb2d8363
fttmatteo <valenciaardila988@icloud.com>
2025-09-23 18:57:29 -0500

    delete and modified all

--- clinic/src/main/java/app/infrastructure/persistence/repository/InvoiceRepository.java
+++
@@ -1,4 +0,0 @@
-package app.infrastructure.persistence.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import app.infrastructure.persistence.entities.InvoiceEntity;
-public interface InvoiceRepository extends JpaRepository<InvoiceEntity, Long> {}
