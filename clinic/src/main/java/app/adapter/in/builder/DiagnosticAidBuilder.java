d0a62ac23fed94482401a8b3fedd592dfb42b550
fttmatteo <valenciaardila988@icloud.com>
2025-09-28 21:25:47 -0500

    AD class in validators - DiagnosticAidBuilder

---
+++ clinic/src/main/java/app/adapter/in/builder/DiagnosticAidBuilder.java
@@ -0,0 +1,26 @@
+package app.adapter.in.builder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import app.adapter.in.validators.DiagnosticAidValidator;
+import app.domain.model.inventory.DiagnosticAid;
+
+/**
+ * Builder para crear instancias de {@link DiagnosticAid} a partir de
+ * cadenas de texto provenientes de solicitudes.  Emplea el
+ * {@link DiagnosticAidValidator} para validar y convertir los campos.
+ */
+@Component
+public class DiagnosticAidBuilder {
+    @Autowired
+    private DiagnosticAidValidator validator;
+
+    public DiagnosticAid build(String id, String name, String cost) throws Exception {
+        DiagnosticAid aid = new DiagnosticAid();
+        aid.setId(validator.idValidator(id));
+        aid.setName(validator.nameValidator(name));
+        aid.setCost(validator.costValidator(cost));
+        return aid;
+    }
+}
